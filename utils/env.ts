49c47299ad38334f9ce21eaafab01e5b90bb866f
Aash1236 <ashutoshfase74@gmail.com>
2026-03-26 23:39:52 +0530

    Playwright automation

---
+++ utils/env.ts
@@ -0,0 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'env/.env'});
+
+export const ENV = {
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+}
